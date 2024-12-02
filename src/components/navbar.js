b22b2300474a76c31951ee9432fa1e20a16a8b8e
govindkrishna03 <govindkrishna28@gmail.com>
2024-12-02 11:57:07 +0530

    fixed build error

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,7 +13,7 @@ const CustomLink = ({ href, title, className = "" }) => {
       {title}
       <span
         className={`h-[0px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
-          router.asPath === href ? "w-full" : "w-0"
+          router.asPath === href ? 'w-full' : 'w-0'
         }`}
       >
         &nbsp;
